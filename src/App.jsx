9022e7f1b0a14830da8740d2a4dfcc07166933dd
dev <dev@test.com>
2025-12-23 20:39:15 +0300

    clear un-needed imports at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { ContextProvider } from "./store/shopping-cart-context";
 import Header from "./components/Header";
 import Product from "./components/Product";
-import Form from "./components/Form";
 
 function App() {
   return (
